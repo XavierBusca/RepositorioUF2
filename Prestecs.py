e383be1525623873bd7cea4bb46da991a7853d86
XavierBusca <novasmurf05@gmail.com>
2024-01-23 14:17:46 +0100

    Hemos hecho la devolucion i el listar esta documentado

--- Prestecs.py
+++ Prestecs.py
@@ -11,18 +11,24 @@ LlibrePrestats = []
 
 while PREGUNTA != 6:
     PREGUNTA = int(input("Que quieres hacer? 1- Añadir, 2- Eliminar, 3- Listar, 4- Anyadir Prestamo, 5- DevolverPrestec. Si quieres salir pulsa cualquier numero diferente a los ya mencionados : "))
-    if PREGUNTA == ANYADIR:
+    if PREGUNTA == 1:
         anyade= input("Escribe el nombre del libro: ")
         Llista.append(anyade)
-    if PREGUNTA == ELIMINAR:
+
+    if PREGUNTA == 2:
         elimina = input("Escribe el nombre del libro que vas a eliminar: ")
-        Llista.pop(elimina)
-    if PREGUNTA == Listar:
-        print(Llista)
-    if PREGUNTA == ANYADIRPRESTEC:
-        anyade3= input("Escribe el nombre del libro: ")
-        LlibrePrestats.append(anyade3)
-    if PREGUNTA == Listar:
-        print(LlibrePrestats)
-    if PREGUNTA == DEVOLVERPRESTEC:
-        
+        Llista.remove(elimina)
+
+    if PREGUNTA == 3:
+        print('Libreria', Llista)
+        print('Libros prestados', LlibrePrestats)
+
+    if PREGUNTA == 4:
+        prestamo= input("Escribe el nombre del libro que quieres hacer el prestamo: ")
+        Llista.remove(prestamo)
+        LlibrePrestats.append(prestamo)
+
+    if PREGUNTA == 5:
+        devolucion= input("Que libro quieres devolver? ")
+        LlibrePrestats.remove(devolucion)
+        Llista.append(devolucion)
