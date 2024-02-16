f4531b6b4649f44f8eb76c04a95f57a0862a9253
XavierBusca <novasmurf05@gmail.com>
2024-02-16 12:18:13 +0100

    acabado

--- frasaeambmesas.py
+++ frasaeambmesas.py
@@ -25,3 +25,47 @@ Té 3 'a'.
 '''
 
 
+MaxAs = 0
+MaxFrase = ""
+fi = False
+
+# Problema general
+def inicio():
+    while not fi:
+        mostrar_menu()
+        tratar_opcion()
+
+# Primer nivel de descomposicion
+def mostrar_menu():
+    print("Benvingut a FraseAmbMesAs")
+    print("-------------------------------------")
+    print("[IF] Introduir frase.")
+    print("[FI] Sortir")
+
+def tratar_opcion():
+    frase = input("Opciones: ")
+    if frase.casefold() == "IF".casefold():
+        poner_la_frase()
+    elif frase.casefold() == "FI".casefold():
+        finalizar_ejecucion()
+    else:
+        print("\nOpcion incorrecta.\n")
+
+def poner_la_frase():
+    poner = input("\nEscribe una frase: ").lower()
+    calcular_AS(poner)
+
+def calcular_AS(frase, MaxAs=0):
+    AS = frase.count('a')
+    if AS > MaxAs:
+        MaxAs = AS
+        MaxFrase = frase
+    print(f"\nLa frase {MaxFrase}: ")
+    print(f"Té {MaxAs} 'a'.\n")
+    return MaxAs
+
+def finalizar_ejecucion():
+    global fi
+    fi = True
+
+inicio()
