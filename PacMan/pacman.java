e29b582144b983abe56debbad03db491033f81b8
XavierBusca <novasmurf05@gmail.com>
2024-04-26 11:39:35 +0200

    He hecho que se mueva hacia una direccion y ahora hare que se mueva a todas direcciones

--- PacMan/pacman.java
+++ PacMan/pacman.java
@@ -7,4 +7,11 @@ class Pacman{
     public int pickupBola();
 
     public int pickupBolaEspecial();
+
+    public int moveY
+
+
+    public moureadalt(int moveY){
+        this.moveY += 1;
+    }
 }
