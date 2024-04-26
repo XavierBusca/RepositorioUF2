85abad30bb31cc68f320165acb6f3c9481be1133
XavierBusca <novasmurf05@gmail.com>
2024-04-26 10:40:08 +0200

    all commit

---
+++ BibliotecaV2/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import Biblioteca.Libro
+
+fun main(args: Array<String>) {
+    var libro1: Libro = Libro()
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+}
---
+++ PacMan/pacman.java
@@ -0,0 +1,8 @@
+class Pacman{
+
+    private int move(int newPosX, int newPosY);
+    
+    private int pickupBola();
+
+    private int pickupBolaEspecial();
+}
