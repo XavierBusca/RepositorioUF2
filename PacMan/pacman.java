6d75f6663f76112407db17d5ece59a5d813706c0
XavierBusca <novasmurf05@gmail.com>
2024-04-26 11:42:56 +0200

    Todas las direcciones hechas

--- PacMan/pacman.java
+++ PacMan/pacman.java
@@ -9,9 +9,20 @@ class Pacman{
     public int pickupBolaEspecial();
 
     public int moveY
-
+    public int moveX
 
     public moureadalt(int moveY){
         this.moveY += 1;
+    }
+
+    public moureabaix(int moveY){
+        this.moveY -= 1;
+    }
+
+    public mouredreta(int moveX){
+        this.moveY += 1;
+    }
+    public moureesquerra(int moveX){
+        this.moveX -= 1;
     }
 }
