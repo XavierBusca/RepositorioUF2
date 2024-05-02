07593f0c9d15e01decfe63fa31fac7062b205305
XavierBusca <novasmurf05@gmail.com>
2024-05-02 11:01:05 +0200

    lo e intentado pero ns

---
+++ Gestio JAVA/CalculadoraEstadistiques.java
@@ -0,0 +1,7 @@
+class CalculadoraEstadistiques{
+    public calcularMitjana(double[] notes);
+
+    public calcularMaxim(double[] notes);
+
+    public calcularMinim(double[] notes);
+}
---
+++ Gestio JAVA/GestorNotes.java
@@ -0,0 +1,10 @@
+class GestorNotes{
+
+    String nomAlumne;
+    double Notes;
+
+    public String registrarNotes(String nomAlumne, double[] Notes);
+
+    public String obtenirNotes(String nomAlumne);
+
+}
