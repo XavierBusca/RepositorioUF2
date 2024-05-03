a9766b8453ae6f36ddb76308304d0a8cf72d4832
XavierBusca <novasmurf05@gmail.com>
2024-05-03 12:24:57 +0200

    Tortura

--- Binari/ArbolEnt.java
+++ Binari/ArbolEnt.java
@@ -1,9 +1,8 @@
-// 1. Crea una nova classe en Java per a la implementació de l'arbre binari.
+
 public class BinaryTree {
     private Node root;
     private static int instances = 0;
 
-    // 2. Implementa constructors per a la creació de l'arbre binari.
     public BinaryTree() {
         this.root = null;
         instances++;
@@ -14,12 +13,10 @@ public class BinaryTree {
         instances++;
     }
 
-    // 3. Volem que les instancies d’arbre sapiguen quantes instancies coexisteixen a la vegada.
     public static int getInstances() {
         return instances;
     }
 
-    // 4. Volem poder fer servir dos tipus d’arbres, un d’enters i un altre de caracters.
     public static class Node {
         private Object data;
         private Node left;
--- Binari/Principal.java
+++ Binari/Principal.java
@@ -1,45 +1,34 @@
 public class Principal {
     public static void main(String[] args) {
-        // A. Crear 2 Arbres d’enters de fondaria 5.
         BinaryTree.Node root1 = new BinaryTree.Node(1, 5);
         BinaryTree tree1 = new BinaryTree(root1);
 
         BinaryTree.Node root2 = new BinaryTree.Node(2, 5);
         BinaryTree tree2 = new BinaryTree(root2);
 
-        // B. Mostrar quantes instancies existeixen d‘arbres.
         System.out.println("Hi ha " + BinaryTree.getInstances() + " instàncies d'arbres.");
 
-        // C. Crear 2 Arbres de caràcters de fondaria 3.
         BinaryTree.Node root3 = new BinaryTree.Node('A', 3);
         BinaryTree tree3 = new BinaryTree(root3);
 
         BinaryTree.Node root4 = new BinaryTree.Node('B', 3);
         BinaryTree tree4 = new BinaryTree(root4);
 
-        // D. Mostrar quantes instancies existeixen d’arbres.
         System.out.println("Hi ha " + BinaryTree.getInstances() + " instàncies d'arbres.");
 
-        // E. Forçar al Garbage Collector.
         System.gc();
 
-        // F. Recorrer el primer arbre d’enters i mostrant els valors per pantalla, fent servir unica i exclusivament les branques dretes.
         traverseRightBranch(tree1.getRoot());
 
-        // G. Recorrer el segon arbre d’enters i mostrant els valors per pantalla, fent servir unica i exclusivament les branques esquerres.
         traverseLeftBranch(tree2.getRoot());
 
-        // H. Tornar a mostrar quantes instancies existeixen.
         System.out.println("Hi ha " + BinaryTree.getInstances() + " instàncies d'arbres.");
 
-        // I. Tornar a forçar al Garbage Collector.
         System.gc();
 
-        // J. Tornar a mostrar quantes instancies existeixen.
         System.out.println("Hi ha " + BinaryTree.getInstances() + " instàncies d'arbres.");
     }
 
-    // F. Recorrer el primer arbre d’enters i mostrant els valors per pantalla, fent servir unica i exclusivament les branques dretes.
     public static void traverseRightBranch(BinaryTree.Node node) {
         if (node != null) {
             traverseRightBranch(node.getRight());
@@ -47,7 +36,6 @@ public class Principal {
         }
     }
 
-    // G. Recorrer el segon arbre d’enters i mostrant els valors per pantalla, fent servir unica i exclusivament les branques esquerres.
     public static void traverseLeftBranch(BinaryTree.Node node) {
         if (node != null) {
             traverseLeftBranch(node.getLeft());
