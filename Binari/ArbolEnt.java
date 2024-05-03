f942c047c768ed7fd8b5b9ce434f8748ae14f05b
XavierBusca <novasmurf05@gmail.com>
2024-05-03 12:15:22 +0200

    Estoy cojiendo codigos tde todos lados

--- Binari/ArbolEnt.java
+++ Binari/ArbolEnt.java
@@ -1,1 +1,36 @@
-class ArbolEnt{}
+// 1. Crea una nova classe en Java per a la implementació de l'arbre binari.
+public class BinaryTree {
+    private Node root;
+    private static int instances = 0;
+
+    // 2. Implementa constructors per a la creació de l'arbre binari.
+    public BinaryTree() {
+        this.root = null;
+        instances++;
+    }
+
+    public BinaryTree(Node root) {
+        this.root = root;
+        instances++;
+    }
+
+    // 3. Volem que les instancies d’arbre sapiguen quantes instancies coexisteixen a la vegada.
+    public static int getInstances() {
+        return instances;
+    }
+
+    // 4. Volem poder fer servir dos tipus d’arbres, un d’enters i un altre de caracters.
+    public static class Node {
+        private Object data;
+        private Node left;
+        private Node right;
+        private int maxDepth;
+
+        public Node(Object data, int maxDepth) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+            this.maxDepth = maxDepth;
+        }
+    }
+}
