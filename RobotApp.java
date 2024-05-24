3a114671654fe7175d389b99e7ac869a94370c56
XavierBusca <novasmurf05@gmail.com>
2024-05-24 10:41:09 +0200

    Lo que he hecho hasta ahora es basicamente crear los botones, crear la consola donde ser veran los msg y el robot, tmb e metido los imports

---
+++ RobotApp.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+public class RobotApp extends JFrame {
+    private JButton EndevantButton, EnrereButton, IzquierdaButton, DretaButton, GuardarEstatButton, CarregarEstatButton ;
+    private JTextArea consoleOutput;
+    private Robot robot;
+}
