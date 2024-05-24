3c908edddfdd9848f229ffbf92efa9d7634207d4
XavierBusca <novasmurf05@gmail.com>
2024-05-24 12:23:44 +0200

    se me a olvidado ir haciendo commits ;( aunque tampoco es que haya hecho mucho mas, he mostrados los botones he hecho que salga por pantalla i poco mas, no he sabido hacer mas

--- RobotApp.java
+++ RobotApp.java
@@ -3,10 +3,10 @@ import java.awt.*;
 import java.io.*;
 
 public class RobotApp extends JFrame {
-    private JButton EndevantButton, EnrereButton, IzquierdaButton, DretaButton, GuardarEstatButton, CarregarEstatButton ;
+    private JButton AdelanteButton, AtrasButton, GirarEButton, GirarDButton, GuardarEstatButton, CarregarEstatButton;
     private JTextArea consoleOutput;
+    private JScrollPane scrollPane;
     private Robot robot;
-}
 
     public RobotApp() {
         setTitle("Control de Robot");
@@ -24,3 +24,54 @@ public class RobotApp extends JFrame {
         controlPanel.add(AtrasButton);
         controlPanel.add(GirarEButton);
         controlPanel.add(GirarDButton);
+
+        consoleOutput = new JTextArea(10, 30);
+        consoleOutput.setEditable(false);
+        scrollPane = new JScrollPane(consoleOutput);
+
+        add(controlPanel, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+
+        AdelanteButton.addActionListener(e -> {
+            robot.AdelanteButton();
+            consoleOutput.append("Robot s'ha mogut endavant.\n");
+        });
+
+        AtrasButton.addActionListener(e -> {
+            robot.AtrasButton();
+            consoleOutput.append("Robot s'ha mogut enrere.\n");
+        });
+
+        GirarEButton.addActionListener(e -> {
+            robot.GirarEButton();
+            consoleOutput.append("Robot ha girat a l'esquerra.\n");
+        });
+
+        GirarDButton.addActionListener(e -> {
+            robot.GirarDButton();
+            consoleOutput.append("Robot ha girat a la dreta.\n");
+        });
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(RobotApp::new);
+    }
+
+    private class Robot {
+        public void AdelanteButton() {
+        }
+
+        public void AtrasButton() {
+        }
+
+        public void GirarEButton() {
+        }
+
+        public void GirarDButton() {
+        }
+
+    }
+}
